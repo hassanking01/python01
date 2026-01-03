8c0ed721ec9adb2387282665d2c66bcc6d86eed4
Hasan Ahchtar <hahchtar@c1r5p9.1337.ma>
2026-01-03 16:05:33 +0100

    fix some

--- ex3/ft_plant_factory.py
+++ ex3/ft_plant_factory.py
@@ -4,9 +4,11 @@ class Plant:
         self.height = height
         self.age = age
 
+    def get_info(self):
+        return f"{self.name} ({self.height}cm, {self.age} days)"
+
 if __name__ == "__main__":
     print("=== Plant Factory Output ===")
-    plants = []
     plants_data = [["Rose" , 25 , 30],
                    ["Oak" ,200 , 365],
                    ["Cactus", 5, 90],
@@ -14,10 +16,8 @@ if __name__ == "__main__":
                    ["Fern" , 15 , 120]
                    ]
     i = 0;
-    for data in plants_data:
-        p1 = Plant(data[0] , data[1] , data[2])
-        plants.append(p1)
+    for name, height, age in plants_data:
+        p1 = Plant(name , height , age)
+        print("Created:" , p1.get_info())
         i += 1;
-    for plant in plants:
-        print("Created:" , f"{plant.name} ({plant.height}cm, {plant.age} days)")
     print("\nTotal plants created:", i)
